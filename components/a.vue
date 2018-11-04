21134b3f5c9f1c6bb2713c8810ec4b2138866053
weiwang <wangweile@shuidihuzhu.com>
2018-11-04 22:39:32 +0800

    修改

---
+++ components/a.vue
@@ -0,0 +1,29 @@
+<template>
+ <div class="name">
+     <div class="name_clacc">
+         <div>新的尝试</div>
+     </div>
+ </div>
+    
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    a() {
+      console.log("100");
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.name {
+  &_clacc {
+    width: 100px;
+  }
+}
+</style>
+
